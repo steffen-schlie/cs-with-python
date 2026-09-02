f9692a67244567ffb4d8339ae2b9104f99cd0fc1
Steffen <ste.schlie@outlook.com>
2026-09-02 20:09:53 +0200

    Finished Lecture 1 and PS0

---
+++ lectures/lecture-1.py
@@ -0,0 +1,12 @@
+# Lecture 1
+
+# Finger Exercises
+
+# Assume 3 variables are already defined for you: a, b and c.
+# Create a variable called total that adds a and b then multiplies
+# the result by c. Include the lase line in your code to print the 
+# value: print(total)
+
+a, b, c = "Peter", " Parker", 3
+total = (a+b)*c
+print(total)
