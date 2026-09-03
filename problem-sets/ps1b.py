2dfe6f73018e9575b1845ae0f75125d228310c5d
Steffen <ste.schlie@outlook.com>
2026-09-03 17:27:28 +0200

    Finished PS01

--- problem-sets/ps1b.py
+++ problem-sets/ps1b.py
@@ -6,14 +6,28 @@
 ##########################################################################################
 ## Get user input for yearly_salary, portion_saved, cost_of_dream_home, semi_annual_raise below ##
 ##########################################################################################
-
+yearly_salary = float(input("Enter the starting yearly salary: "))
+portion_saved = float(input("Enter a portion saved (as a decimal between 0 and 1): "))
+cost_of_dream_home = float(input("Enter the cost of your dream home: "))
+semi_annnual_raise = float(input("Enter your semi annual raise rate: "))
 
 #########################################################################
 ## Initialize other variables you need (if any) for your program below ##
 #########################################################################
-
+months = 0
+amount_saved = 0
+portion_down_payment = 0.25
+r = 0.05
 
 ###############################################################################################
 ## Determine how many months it would take to get the down payment for your dream home below ## 
 ###############################################################################################
 
+# Add additional condition that checks if semi-year is reached, then only change yearly_salary
+while (cost_of_dream_home*portion_down_payment > amount_saved):
+    months += 1
+    amount_saved += (yearly_salary*portion_saved)/12 + (amount_saved*(r/12))
+    if months%6 == 0:
+        yearly_salary += yearly_salary*semi_annnual_raise
+
+print(f"Number of months to save for down payment on dream house: {months}")
--- problem-sets/ps1c.py
+++ problem-sets/ps1c.py
@@ -6,14 +6,39 @@
 ##############################################
 ## Get user input for initial_deposit below ##
 ##############################################
+initial_deposit = float(input("Enter the initial deposit: "))
 
 
 #########################################################################
 ## Initialize other variables you need (if any) for your program below ##
 #########################################################################
+cost_of_house = 800_000
+down_payment = cost_of_house*0.25
+steps = 0
+r_upper_bound = 1
+r_lower_bound = 0
+best_savings_rate = 0.5
+
 
 
 ##################################################################################################
 ## Determine the lowest rate of return needed to get the down payment for your dream home below ##
 ##################################################################################################
+# Check whether initial amount is already enough
+if initial_deposit >= down_payment:
+    best_savings_rate = 0.0
+elif initial_deposit*(1+1/12)**36 < down_payment - 100:
+    best_savings_rate = None
+else:
+    while (initial_deposit*(1+best_savings_rate/12)**36 < down_payment - 100 
+           or initial_deposit*(1+best_savings_rate/12)**36 > down_payment + 100):
+        steps += 1
+        if initial_deposit*(1+best_savings_rate/12)**36 < down_payment - 100:
+            r_lower_bound = best_savings_rate
+            best_savings_rate = (r_upper_bound + r_lower_bound)/2
+        else:
+            r_upper_bound = best_savings_rate
+            best_savings_rate = (r_upper_bound + r_lower_bound)/2
 
+print(f"Best savings rate: {best_savings_rate}")
+print(f"Steps in bisection search: {steps}")
