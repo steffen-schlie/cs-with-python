40073db27b217db6d60d4935655507726764939c
Steffen <ste.schlie@outlook.com>
2026-09-11 18:05:35 +0200

    Add You-try-it solutions from Lec 8

--- lectures/lec8.py
+++ lectures/lec8.py
@@ -71,4 +71,21 @@ def count_nums_with_sqrt_close_to(n, epsilon):
             print(bisection_root(i))
     return total
 
-print(count_nums_with_sqrt_close_to(10, 0.1))
+# print(count_nums_with_sqrt_close_to(10, 0.1))
+
+# YOU TRY IT
+# Write a function that meets these specs
+def apply(criteria, n):
+    """
+    criteria: is a func that takes in a number and returns a bool
+    n: int
+    Returns how many ints from 0 to n (inclusive) match the criteria 
+    (i.e. return True when run with criteria)
+    """
+    count = 0
+    for i in range(n+1):
+        if criteria(i):
+            count += 1
+    return count
+
+print(apply(is_triangular, 20))
